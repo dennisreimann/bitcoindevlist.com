676002a70f79f5ad56a51cf094fa16ea650c0383
Dennis Reimann <mail@dennisreimann.de>
2020-07-29 19:20:16 +0200

    👨🏻‍💻 Add more devs to list

--- data.js
+++ data.js
@@ -31,6 +31,62 @@ module.exports = {
       donate: "https://github.com/sponsors/sipsorcery",
       avatar: "https://avatars1.githubusercontent.com/u/197660?s=400&u=56479352d085f0d392512768d25957b0b3b770b4&v=4",
       description: "I work on open source C# VoIP & Video + contribute to Bitcoin's cross platform build efforts."
+    },
+    {
+      name: "Samuel Dobson",
+      github: "meshcollider",
+      twitter: "meshcollider",
+      donate: "https://github.com/sponsors/meshcollider",
+      avatar: "https://pbs.twimg.com/profile_images/1230073890749026305/N5tPUIyF_400x400.jpg",
+      description: "I contribute to the open source Bitcoin Core project, where I work as a maintainer. I focus on wallet development, and as part of that, I code, review and merge pull requests into the project when they are ready."
+    },
+    {
+      name: "Luke Dashjr",
+      github: "LukeDashjr",
+      twitter: "LukeDashjr",
+      donate: "https://github.com/sponsors/luke-jr",
+      avatar: "https://pbs.twimg.com/profile_images/1157377276268437504/u1quAN_D_400x400.png",
+      description: "As the longest-contributing Bitcoin Core developer, I've been working on Bitcoin since the start of 2011, primarily involving ongoing development, but also including research (of possible future improvements), community education, handling security issues, etc."
+    },
+    {
+      name: "Kukks",
+      github: "MrKukks",
+      twitter: "MrKukks",
+      donate: "https://btc.donate.kukks.org/",
+      avatar: "https://avatars2.githubusercontent.com/u/1818366?s=400&u=c3db16109eea21ab3bfc626ea0d6146d5a69bd83&v=4",
+      description: "I work full-time on BTCPayServer, an open-source, self-hosted bitcoin payment processor that allows individuals and companies become self-sovereign."
+    },
+    {
+      name: "Luke Childs",
+      github: "lukechilds",
+      twitter: "lukechilds",
+      donate: "https://github.com/sponsors/lukechilds",
+      avatar: "https://avatars2.githubusercontent.com/u/2123375?s=400&u=d37877f1b58fbb434defd598fdd5b044376e3f72&v=4",
+      description: "I've been contributing to the browserify and BitcoinJS projects to bring the latest Bitcoin features to JavaScript. I've also published some libraries and educational sites related to Bitcoin."
+    },
+    {
+      name: "Evan Kaloudis",
+      github: "evankaloudis",
+      twitter: "kaloudis",
+      donate: "https://github.com/sponsors/kaloudis",
+      avatar: "https://avatars1.githubusercontent.com/u/1878621?s=400&u=113b49d841f78df9a25769a5316f34b58816b0aa&v=4",
+      description: "Working on Bitcoin-related projects, first and foremost, the Bitcoin/Lightning mobile wallet Zeus."
+    },
+    {
+      name: "Dennis Reimann",
+      github: "DennisReimann",
+      twitter: "DennisReimann",
+      donate: "https://d11n.net/donate.html",
+      avatar: "https://avatars1.githubusercontent.com/u/886?s=460&u=d0dc12bbe1e126b61078a23d540650b45ea5c204&v=4",
+      description: "Improving the UX/UI of Bitcoin-related projects. Currently working full-time on BTCPay Server – an open-source, self-hosted payment processor for Bitcoin."
+    },
+    {
+      name: "Eskyee",
+      github: "Eskyee",
+      twitter: "Esky33junglist",
+      donate: "https://btcpayjungle.com/apps/iDPaGRfAvo8by73jMpZF83VMQQu/pos",
+      avatar: "https://avatars2.githubusercontent.com/u/30862925?s=400&u=6e901281f8e1c7b73922b99d40526f8fc8c0e9cc&v=4",
+      description: "Working full time for JungleLab, BTCPayServer, & maintaining BTCPayJungle"
     }
   ]
 }
