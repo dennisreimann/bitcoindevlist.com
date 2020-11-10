96f4a4781d3b9b0e9e070a8529ec68d53c8d3543
Dennis Reimann <mail@dennisreimann.de>
2020-11-10 08:25:38 +0100

    Add sitemap

---
+++ tasks/generate_sitemap.js
@@ -0,0 +1,13 @@
+const pug = require('pug')
+const glob = require('glob')
+const { writeFileSync } = require('fs')
+const { resolve } = require('path')
+
+const html = glob.sync(resolve(__dirname, '..', `dist/**/*.html`))
+const pages = html.map(file => file.replace(/.*\/dist/, 'https://bitcoindevlist.com'))
+const now = (new Date()).toISOString()
+const file = resolve(__dirname, '..', `includes/sitemap.pug`)
+const rendered = pug.renderFile(file, { pages, now, pretty: true })
+const dst = resolve(__dirname, '..', 'dist', 'sitemap.xml')
+
+writeFileSync(dst, rendered)
